ed963d2c3c4feb9a958d1677cc87f8e764202224
InFav <innovations.arz@gmail.com>
2024-11-25 01:31:14 -0600

    Update CORS for deployment

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ init_firebase()
 
 origins = [
     "http://localhost:5173",
-    "https://navigatorhub.netlify.app"
+    "https://navigatorhub.netlify.app",
+    "https://navhub-backend-3bcc3222593e.herokuapp.com"
 ]
 
 app.add_middleware(
